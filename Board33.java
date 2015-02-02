d36e10f85ea52b83d642b95991444fe4b475faf9
Drew Guild <drew.guild@gmail.com>
2015-02-02 14:21:31 -0500

    Copied methods over to Board33

--- Board33.java
+++ Board33.java
@@ -30,6 +30,59 @@ public class Board33{
       holes[16].changeHole(); 
    }
    
+   //Counts the remaining pegs on the board
+   public int countPegs(){
+      int count = 0;
+      for(Hole hole : holes){
+         if(!hole.isEmpty()){
+            count++;
+         }
+      }
+      return count;
+   }
+   
+   //Displays the board in ASCII representation
+   public void displayBoard(){
+      ;
+   }
+   
+   //Move peg from fromHole to toHole
+   public void move(Hole fromHole, Hole toHole){
+      if(!isMoveLegal(fromHole, toHole)){
+         System.out.println("Move is not legal");
+      }
+      else{
+         //change the states of each involved hole
+         fromHole.changeHole();
+         toHole.changeHole();
+         findMiddleHole(fromHole,toHole).changeHole();
+      }
+   }
+   
+   
+   
+   //Checks if the move is allowed
+   private boolean isMoveLegal(Hole fromHole, Hole toHole){
+      return (isHoleTwoAway(fromHole, toHole) && !(findMiddleHole(fromHole, toHole).isEmpty()));
+   }
+   
+   //Finds the hole that is between two holes
+   private Hole findMiddleHole(Hole h1, Hole h2){
+      //initialized to dummy hole 
+      Hole middleHole = new Hole(0,0,false);
+      
+      int middleHoleRow = (h1.row + h2.row)/2;
+      int middleHoleColumn = (h1.column + h2.column)/2;
+      //  search board array for hole with this coordinate
+      for(Hole h: holes){
+         if(h.row == middleHoleRow && h.column == middleHoleColumn){
+            middleHole = h;
+         }
+      }
+      
+      return middleHole;
+   }
+   
    
    private boolean isHoleTwoAway(Hole h1, Hole h2){
       // Difference between holes must be 2 with either row distance or column distance being 2
